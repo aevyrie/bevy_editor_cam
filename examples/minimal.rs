41689222c1ce96f6b071c89a3008d05ccd83c675
Duncan Fairbanks <duncanfairbanks6@gmail.com>
2024-12-04 23:11:41 -0800

    feat: upgrade minimal example

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -2,13 +2,14 @@
 
 use bevy::prelude::*;
 use bevy_editor_cam::prelude::*;
+use indoc::indoc;
 
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins,
-            bevy_mod_picking::DefaultPickingPlugins, // Prerequisite: Use picking plugin
-            DefaultEditorCamPlugins,                 // Step 1: Add camera controller plugin
+            MeshPickingPlugin,
+            DefaultEditorCamPlugins, // Step 1: Add camera controller plugin
         ))
         .add_systems(Startup, (setup_camera, setup_scene))
         .run();
@@ -23,6 +24,7 @@ fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
             intensity: 1000.0,
             diffuse_map: asset_server.load("environment_maps/diffuse_rgb9e5_zstd.ktx2"),
             specular_map: asset_server.load("environment_maps/specular_rgb9e5_zstd.ktx2"),
+            rotation: default(),
         },
     ));
 }
@@ -33,26 +35,21 @@ fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 fn setup_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(SceneBundle {
-        scene: asset_server.load("models/PlaneEngine/scene.gltf#Scene0"),
+        scene: SceneRoot(asset_server.load("models/PlaneEngine/scene.gltf#Scene0")),
         transform: Transform::from_xyz(0.0, -0.5, -2.0),
         ..Default::default()
     });
 
-    let style = TextStyle {
-        font_size: 20.0,
-        ..default()
-    };
-    commands.spawn(
-        TextBundle::from_sections(vec![
-            TextSection::new("Left Mouse - Pan\n", style.clone()),
-            TextSection::new("Right Mouse - Orbit\n", style.clone()),
-            TextSection::new("Scroll - Zoom\n", style.clone()),
-        ])
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            top: Val::Px(12.0),
-            left: Val::Px(12.0),
+    let text = indoc! {"
+        Left Mouse - Pan
+        Right Mouse - Orbit
+        Scroll - Zoom
+    "};
+    commands.spawn((
+        Text::new(text),
+        TextFont {
+            font_size: 20.0,
             ..default()
-        }),
-    );
+        },
+    ));
 }
